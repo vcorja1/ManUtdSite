2027866635e656ece51f6533adb77a11ebf607ce
Vladimir Corja <v.corja@yahoo.com>
2018-02-08 20:26:54 -0500

    Restructured pages and route logic

--- routes/standings.js
+++ routes/standings.js
@@ -2,15 +2,34 @@
 var express = require('express');
 var app = express();
 
-// Connect and use the middleware
+// Require the middleware
 const standings = require('../middleware/standings');
-app.use('/', standings.getEPLTable);
+
+// GET response for '/standings/premier-league'
+app.use('/premier-league', standings.getEPLTable);
+
+app.get('/premier-league', function(req, res, next) {
+
+	try {
+		res.render('standings/pl_standings', {
+			title: 'Premier League Standings',
+			epl: req.epl
+		});
+	}
+	catch (e) {
+		// If there are any errors, send them off the the logger
+		next(e);
+	}
+
+});
 
 // GET response for '/standings'
+app.use('/', standings.getEPLTable);
+
 app.get('/', function(req, res, next) {
 
 	try {
-		res.render('standings', {
+		res.render('standings/standings', {
 			title: 'Standings',
 			epl: req.epl
 		});
